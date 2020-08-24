560f17fe7be1c3e0252ff265039b5f8a520d9d21
Mary <phunmung@gmail.com>
2020-08-25 04:39:17 +0900

    Upload Project

---
+++ Natural Selection/Assets/PlayerControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] private GameObject foodToSpawn;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePositionInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (CurrentScene.Instance().GetCollider().ClosestPoint(mousePositionInWorld) == mousePositionInWorld)
+            {
+                Instantiate(foodToSpawn, mousePositionInWorld, Quaternion.identity);
+            }
+        }
+    }
+}
